b9c182cb5afa26d434e12b6de5cc18233900370f
coderaiser <coderaiser@cloudcmd.io>
2016-11-15 16:12:15 +0200

    chore(alisa) rm iife

--- bin/alisa.js
+++ bin/alisa.js
@@ -1,47 +1,45 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var spawnify    = require('spawnify'),
+    argv        = require('minimist')(process.argv.slice(2)),
+    platform    = process.platform;
+
+switch(platform) {
+case 'win32':
+    exec(argv.win);
+    break;
+
+case 'linux':
+    exec(argv.linux);
+    break;
+
+case 'darwin':
+    exec(argv.darwin);
+    break;
+
+case 'sunos':
+    exec(argv.sunos);
+    break;
+}
+
+function exec(cmd) {
+    var spawn;
     
-    var spawnify    = require('spawnify'),
-        argv        = require('minimist')(process.argv.slice(2)),
-        platform    = process.platform;
-    
-    switch(platform) {
-    case 'win32':
-        exec(argv.win);
-        break;
-    
-    case 'linux':
-        exec(argv.linux);
-        break;
-    
-    case 'darwin':
-        exec(argv.darwin);
-        break;
-    
-    case 'sunos':
-        exec(argv.sunos);
-        break;
-    }
-    
-    function exec(cmd) {
-        var spawn;
+    if (cmd) {
+        spawn = spawnify(cmd);
         
-        if (cmd) {
-            spawn = spawnify(cmd);
-            
-            spawn.on('data', function(data) {
-                process.stdout.write(data);
-            });
-            
-            spawn.on('error', function(error) {
-                console.error(error.message);
-            });
-            
-            spawn.on('exit', function() {
-                spawn = null;
-            });
-        }
+        spawn.on('data', function(data) {
+            process.stdout.write(data);
+        });
+        
+        spawn.on('error', function(error) {
+            console.error(error.message);
+        });
+        
+        spawn.on('exit', function() {
+            spawn = null;
+        });
     }
-})();
+}
